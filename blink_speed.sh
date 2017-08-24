3e07b8fcd0770a612bf137f4b5a318c8e54f6b2c
david <sopkin.sf@gmail.com>
2017-08-23 20:10:28 -0700

    Explain what blink(1) is

--- blink_speed.sh
+++ blink_speed.sh
@@ -1,4 +1,6 @@
-# This is an internet speed utility for the blink(1) light.
+# This is an internet speed utility for the blink(1) USB RGB light. 
+# Learn more about the blink(1) here: 
+# https://blink1.thingm.com/
 
 
 speed='speedtest-cli | grep Download | cut -c 11-15'; if [ $speed -lt "10" ]; then blink1 --orange blink 10 && echo "hella slow" && echo $speed; else blink1 --green blink n10 && echo "hella fast" && echo $speed; fi; blink1 --off
