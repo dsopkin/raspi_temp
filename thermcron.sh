0a3ab24c73e353164bac23073e165c816f8a9c05
dsopkin <sopkin.sf@gmail.com>
2017-11-02 21:23:10 -0700

    - remove blink_speed.sh script (moved to gist) - small grammatical changes in thermcron.sh

--- thermcron.sh
+++ thermcron.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# Cronjob script to run as oftenas you want, will add a line to the CSV.
+# Cronjob script to run as often as you want, will add a line to the CSV.
 
 python /root/thermo.py >> /root/temp_log.csv
